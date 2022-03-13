ae63b63c47cc0e307e1fb240df52923b19c55fbb
channpreet1991 <65122644+channpreet1991@users.noreply.github.com>
2022-03-13 11:14:44 -0300

    2nd commit

--- Price-Quote Application Assignment.cs
+++ Price-Quote Application Assignment.cs
@@ -1,24 +1,20 @@
-#include <stdio.h>
-int main() {
-   float weight, width, height, length, quote;
-   printf("Welcome to Package Express. Please follow the instructions below.\n");
-   printf("Please enter the package weight:\n");
-   scanf("%f", &weight);
-   if(weight > 50) {
-       printf("Package too heavy to be shipped via Package Express. Have a good day.");
-       return 0;
-   }
-   printf("Please enter the package width:\n");
-   scanf("%f", &width);
-   printf("Please enter the package height:\n");
-   scanf("%f", &height);
-   printf("Please enter the package length:\n");
-   scanf("%f", &length);
-   if(width+height+length > 50) {
-       printf("Package too big to be shipped via Package Express.");
-       return 0;
-   }
-   quote = width*height*length*weight/100;
-   printf("Your estimated total for shipping this package is: $%.2f\n Thank you.", quote);
-   return 0;
-}
+int length,width,height,weight; 
+ 
+Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");  
+Console.WriteLine("Enter weight");
+ 
+if weight>50  
+Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");  
+ 
+
+Console.WriteLine("Choose an option from the following list:");  
+Console.WriteLine("\ta -Enter Width");  
+Console.WriteLine("\ts -Enter  Height");  
+Console.WriteLine("\tm -Enter Length"); 
+if length + width+ height > 50 
+  
+Console.Write("Package too big to be shipped via Package Express.");  
+
+res=((length*width*height)*weight)/100;
+
+Console.WriteLine("$"res);
